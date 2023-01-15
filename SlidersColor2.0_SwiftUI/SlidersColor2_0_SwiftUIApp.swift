66359ad5d54d902d651fe82ceb581a0bdac28483
grizzlyq <pechenika73@yandex.ru>
2023-01-15 21:36:13 +0300

    Initial Commit

---
+++ SlidersColor2.0_SwiftUI/SlidersColor2_0_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersColor2_0_SwiftUIApp.swift
+//  SlidersColor2.0_SwiftUI
+//
+//  Created by Дмитрий on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlidersColor2_0_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
